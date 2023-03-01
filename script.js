7bb4c451f68d70d0f19f35b6b1d3f8d678f4a648
Daniel Alexander Dam <dani36i9@stud.kea.dk>
2023-03-01 22:28:07 +0100

    fucking collision!!!

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function () {
     //canvas setup
-    const canvas = document.getElementById('canvas1')
-    const ctx = canvas.getContext("2d")
-    canvas.width = 500
-    canvas.height = 500
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext("2d");
+    canvas.width = 500;
+    canvas.height = 500;
 
     //syntcatical sugar - syntax within a programming language that is designed to make things easier to read or to express
 
@@ -12,13 +12,13 @@ window.addEventListener('load', function () {
             this.game = game;
             window.addEventListener('keydown', e => {
                 // only add if unique
-                if ((e.key === 'ArrowUp') ||
+                if (((e.key === 'ArrowUp') ||
                     (e.key === 'ArrowDown')
-
-                    && this.game.keys.indexOf(e.key) === -1) {
+                ) && this.game.keys.indexOf(e.key) === -1) {
                     this.game.keys.push(e.key);
+                } else if (e.key === ' ') {
+                    this.game.player.shootTop();
                 }
-                console.log(this.game.keys);
             })
             // remove any if already present
             window.addEventListener('keyup', e => {
@@ -47,7 +47,8 @@ window.addEventListener('load', function () {
             this.x = 20;
             this.y = 100;
             this.speedY = 0;
-            this.maxSpeed = 2;
+            this.maxSpeed = 2.5;
+            this.projectiles = [];
         }
 
         update() {
@@ -60,12 +61,33 @@ window.addEventListener('load', function () {
         }
 
         draw(context) {
-            context.fillRect(this.x, this.y, this.width, this.height)
+            context.fillStyle = "black";
+            context.fillRect(this.x, this.y, this.width, this.height);
+            this.projectiles.forEach(projectiles => {
+                projectiles.draw(context);
+            });
         }
 
+        //passer character/player koordinater fra Player constructor: x & y
+        shootTop() {
+            if (this.game.ammo > 0) {
+                this.projectiles.push(new Projectile(this.game, this.x + 80, this.y + 30));
+                this.game.ammo--;
+            }
+        }
     }
 
     class Enemy {
+
+    }
+
+    class Angler1 extends Enemy {
+        constructor(game) {
+            super(game);
+            this.width = 228 * 0.2;
+            this.height = 169 * 0.2;
+            this.y = Math.random() * (this.game.height * 0.9 - this.height);
+        }
 
     }
 
@@ -87,25 +109,87 @@ window.addEventListener('load', function () {
             this.height = height;
             this.player = new Player(this);
             this.input = new InputHandler(this);
+            this.ui = new UI(this);
             this.keys = [];
+            this.enemies = [];
+            this.enemyTimer = 0;
+            this.enemyInterval = 1000;
+            this.ammo = 20;
+            this.maxAmmo = 50;
+            this.ammoTimer = 0;
+            this.ammoInterval = 500;
+            this.gameOver = false;
+            this.score = 0;
+            this.winningScore = 10;
+
         }
 
-        update() {
+        update(deltaTime) {
+            // count up ammoTimer and reset when reached ammoInterval
             this.player.update()
+            if (this.ammoTimer > this.ammoInterval) {
+                if (this.ammo < this.maxAmmo) this.ammo++;
+                this.ammoTimer = 0;
+            } else {
+                this.ammoTimer += deltaTime;
+            }
+
+            // iterate over enemies for different checks
+            this.enemies.forEach(enemy => {
+                enemy.update();
+                if (this.checkCollision(this.player, enemy)) {
+                    enemy.markedForDeletion = true;
+                }
+                this.player.projectiles.forEach(projectile => {
+                    if (this.checkCollision(projectile, enemy)){
+                        enemy.lives--;
+                        projectile.markedForDeletion = true;
+                        if (enemy.lives <= 0){
+                            enemy.markedForDeletion = true;
+                            this.score += enemy.score;
+                            if(this.score > this.winningScore) this.gameOver = true;
+                        }
+                    }
+                });
+            });
+            this.enemies = this.enemies.filter(enemy => !enemy.markedForDeletion);
+            if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+                this.addEnemy();
+                this.enemyTimer = 0;
+            } else {
+                this.enemyTimer += deltaTime;
+            }
         }
 
         draw(context) {
-            this.player.draw(context)
+            this.player.draw(context);
+            this.enemies.forEach(enemy => {
+                enemy.draw(context);
+            });
+            this.ui.draw(context);
+        }
+
+        addEnemy() {
+            this.enemies.push(new Angler1(this));
+            console.log(this.enemies);
+        }
+
+        checkCollision(rect1, rect2) {
+            return (rect1.x < rect2.x + rect2.width &&
+                    rect1.x + rect1.width > rect2.x &&
+                    rect1.y < rect2.y + rect2.height &&
+                    rect1.height + rect1.y > rect2.y)
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
 
     // animation loop
     function animate() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        game.update()
-        game.draw(ctx)
+        game.update(deltaTime);
+        game.draw(ctx);
         //tells the browser we wish to perform an animation and requests that that
         //browser calls a specified function to update an animation before the next repaint.
         requestAnimationFrame(animate)
